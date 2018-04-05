593f82846fa43e1161673261dcccf6fb4aa8c252
Nicholas Istre <nickistre@gmail.com>
2018-04-05 16:06:53 -0700

    Initial working Pomodoro Timer

--- js/app.js
+++ js/app.js
@@ -1,34 +1,117 @@
-// Will just use basic jQuery
-jQuery(function() {
-    function poll(currentStatus) {
-        console.log(currentStatus);
+function splitTimer(timer) {
+    return {
+        minutes: parseInt(timer / 60, 10),
+        seconds: parseInt(timer % 60, 10)
+    };
+}
+
+/**
+ *
+ * @param number
+ * @param size
+ * @returns {string}
+ * {@link https://stackoverflow.com/a/2998822/1946899|Modifed from Source}
+ */
+function zeroPad(number, size) {
+    var s=number+'';
+    while (s.length < size) {
+        s = '0' + s;
     }
 
-    var pomodoroTimer = new PomodoroTimer(poll);
+    return s;
+}
 
-    // Debug timer system with alternate timings
-    pomodoroTimer.timerPatterns = [
-        {
-            workTime: 10,
-            restMinTime: 5,
-            restMaxTime: 10
-        },
-        {
-            workTime: 10,
-            restMinTime: 5,
-            restMaxTime: 10
-        },
-        {
-            workTime: 10,
-            restMinTime: 5,
-            restMaxTime: 10
-        },
-        {
-            workTime: 10,
-            restMinTime: 20,
-            restMaxTime: 30
+/**
+ *
+ * @param string
+ * @returns {string}
+ * {@link https://stackoverflow.com/a/1026087/1946899|Source}
+ */
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function poll(currentStatus) {
+    console.log(currentStatus);
+
+    if (currentStatus.status == PomodoroTimer.STATUS_PAUSED) {
+        jQuery('#control-btn').html('Start');
+        jQuery('#timer-container').removeClass('running');
+        jQuery('#timer-container').addClass('paused');
+    }
+    else {
+        jQuery('#control-btn').html('Pause');
+        jQuery('#timer-container').removeClass('paused');
+        jQuery('#timer-container').addClass('running');
+    }
+
+    // Update main timer display
+    var timerSplit = splitTimer(currentStatus.totalSeconds);
+    jQuery('#main-timer .minutes').html(zeroPad(timerSplit.minutes, 2));
+    jQuery('#main-timer .seconds').html(zeroPad(timerSplit.seconds, 2));
+
+    // Show mode
+    if (currentStatus.status == PomodoroTimer.STATUS_PAUSED) {
+        jQuery('#mode-display').html('<em>Timer Paused</em>')
+    }
+    else {
+        jQuery('#mode-display').html(capitalizeFirstLetter(currentStatus.modeString));
+    }
+
+    // Setup next button and effects
+    if (currentStatus.canStartNextRound) {
+        // Enable next button
+        jQuery('#next-round-btn').prop('disabled', false);
+    }
+    else {
+        jQuery('#next-round-btn').prop('disabled', true);
+    }
+}
+
+var pomodoroTimer = new PomodoroTimer(poll);
+
+// Debug timer system with alternate timings
+pomodoroTimer.setTimerPatterns([
+    {
+        workTime: 10,
+        restMinTime: 5,
+        restMaxTime: 10
+    },
+    {
+        workTime: 10,
+        restMinTime: 5,
+        restMaxTime: 10
+    },
+    {
+        workTime: 10,
+        restMinTime: 5,
+        restMaxTime: 10
+    },
+    {
+        workTime: 10,
+        restMinTime: 20,
+        restMaxTime: 30
+    }
+]);
+
+// Will just use basic jQuery
+
+jQuery(function() {
+
+    jQuery('#control-btn').click(function() {
+        if (pomodoroTimer.getStatus() == PomodoroTimer.STATUS_PAUSED) {
+            pomodoroTimer.resume();
         }
-    ];
+        else {
+            pomodoroTimer.pause();
+        }
+    });
 
-    pomodoroTimer.start();
+    jQuery('#reset-btn').click(function() {
+        pomodoroTimer.reset();
+    });
+
+    jQuery('#next-round-btn').click(function () {
+        pomodoroTimer.next();
+    })
 });
--- js/classes/pomodoro-timer.js
+++ js/classes/pomodoro-timer.js
@@ -1,114 +1,15 @@
 /**
  *
- * @param pollFunc Function that will trigger on each timer update.  Current timer values and status will be provided
+ * @param updateCallback Function that will trigger on each timer update.  Current timer values and status will be provided
  * as a single object parameter.
  * @constructor
  */
-function PomodoroTimer(pollFunc) {
-
-    // Internal classes.
-
-    /**
-     * Class to use for each round
-     *
-     * TODO: Move this functionality into main class!
-     *
-     * @param workTime
-     * @param restMinTime
-     * @param restMaxTime
-     * @param pollFunc Returns the current RoundTimer
-     * @constructor
-     */
-    function RoundTimer(workTime, restMinTime, restMaxTime, pollFunc) {
-        // Constants used internally by the timer
-        var STATUS_START = 0;
-        var STATUS_WORK = 1;
-        var STATUS_REST = 2;
-        var STATUS_COMPLETE = 3;
-
-        var STATUS_STRINGS = {
-            0: 'paused',
-            1: 'work',
-            2: 'rest',
-            3: 'complete'
-        };
-
-        var status = STATUS_START;
-
-        // Public parameters
-        this.workTime = workTime;
-        this.restMinTime = restMinTime;
-        this.restMaxTime = restMaxTime;
-        this.timer = null;
-        this.canStartNextRound = false;
-
-        this.minutes = null;
-        this.seconds = null;
-
-        this.displayMinutes = null;
-        this.displaySeconds = null;
-
-        // Public Methods
-        this.reset = function() {
-            this.status = STATUS_START;
-            this.canStartNextRound = false;
-        };
-
-        this.statusString = function() {
-            return STATUS_STRINGS[status];
-        };
-
-        /**
-         * Modified much of this function from this jsfiddle: http://jsfiddle.net/wr1ua0db/17/
-         */
-        this.start = function () {
-            status = STATUS_WORK;
-            this.timer = this.workTime;
-            this.canStartNextRound = false;
-
-            this.resume();
-        };
-
-        this.resume = function() {
-            var self = this;
-            var intervalId = window.setInterval(function()
-            {
-                self.minutes = parseInt(self.timer / 60, 10)
-                self.seconds = parseInt(self.timer % 60, 10);
-
-                self.displayMinutes = self.minutes < 10 ? "0" + self.minutes : self.minutes.toString();
-                self.displaySeconds = self.seconds < 10 ? "0" + self.seconds : self.seconds.toString();
-
-                pollFunc(self);
-
-                if (--self.timer <= 0) {
-                    if (status == STATUS_WORK) {
-                        // Reset for restMaxTime
-                        status = STATUS_REST;
-                        self.timer = self.restMaxTime;
-                    }
-                    else if (status == STATUS_REST) {
-                        // We are done, start the next round!
-                        status = STATUS_COMPLETE;
-                        window.clearInterval(intervalId);
-                        // Trigger the poll timer for the complete status
-                        pollFunc(self);
-                    }
-                }
-
-                // If in rest mode, check for if we can start the next round once restMinTime was reached.
-                if (status == STATUS_REST) {
-                    // Check if timer is less than the difference between restMaxTime and restMinTime
-                    if (self.timer < (self.restMaxTime - self.restMinTime)) {
-                        self.canStartNextRound = true;
-                    }
-                }
-            }, 1000)
-        };
-
-        // Init
-        this.reset();
-    }
+function PomodoroTimer(updateCallback) {
+    // Internal variables
+    this.mode = null;
+    this.status = null;
+    this.intervalId = null;
+    this.updateCallback = updateCallback;
 
     // Public parameters
     /**
@@ -143,66 +44,168 @@ function PomodoroTimer(pollFunc) {
      * @type {number}
      */
     this.roundCount = 0;
-    /**
-     * @type {RoundTimer}
-     */
-    this.currentRoundTimer = null;
 
-    // Internal Methods
-    /**
-     * Should only run on reset or when needing to go to the next round.
-     */
-    this.updateCurrentRoundTimer = function()
-    {
-        this.roundCount++;
+    this.workTime = null;
+    this.restMinTime = null;
+    this.restMaxTime = null;
+    this.timer = null;
+    this.canStartNextRound = false;
 
-        var patternIndex = this.roundCount % this.timerPatterns.length;
-        var pattern = this.timerPatterns[patternIndex];
+    this.minRestTimer = null;
 
-        var self = this;
-        currentRoundTimer = new RoundTimer(pattern.workTime, pattern.restMinTime, pattern.restMaxTime, function (roundTimer) {
-            // If complete, need to move to the next round
-            if (roundTimer.statusString() == 'complete') {
-                self.updateCurrentRoundTimer();
-            }
-            else {
-                var currentStatus = {
-                    minutes: roundTimer.minutes,
-                    seconds: roundTimer.seconds,
-                    displayMinutes: roundTimer.displayMinutes,
-                    displaySeconds: roundTimer.displaySeconds,
-                    totalSeconds: roundTimer.timer,
-                    canStartNextRound: roundTimer.canStartNextRound,
-                    status: roundTimer.statusString(),
-                    roundCount: self.roundCount
-                }
-
-                pollFunc(currentStatus);
-            }
-        });
-    }
-
-
-    // Public Methods
-
-    this.reset = function() {
-        roundCount = 0;
-    }
-
-    /**
-     *
-     * @returns {string}
-     */
-    this.status = function() {
-        return currentRoundTimer.status();
-    }
-
-    this.start = function() {
-        this.updateCurrentRoundTimer();
-        this.reset();
-        currentRoundTimer.start();
-    }
 
     // Init instance
     this.reset();
+}
+
+// Constants
+PomodoroTimer.MODE_WORK = 0;
+PomodoroTimer.MODE_REST = 1;
+
+PomodoroTimer.MODE_STRINGS = {
+    0: 'work',
+    1: 'rest'
+};
+
+PomodoroTimer.STATUS_PAUSED = 0;
+PomodoroTimer.STATUS_RUNNING = 1;
+
+PomodoroTimer.STATUS_STRINGS = {
+    0: 'paused',
+    1: 'running'
+};
+
+// Internal Methods
+/**
+ * Should only run on reset or when needing to go to the next round.
+ */
+PomodoroTimer.prototype.setupNextRound = function()
+{
+    this.roundCount++;
+
+    var patternIndex = (this.roundCount - 1) % this.timerPatterns.length;
+    var pattern = this.timerPatterns[patternIndex];
+
+    this.workTime = pattern.workTime;
+    this.restMinTime = pattern.restMinTime;
+    this.restMaxTime = pattern.restMaxTime;
+
+    this.timer = this.workTime;
+    this.mode = PomodoroTimer.MODE_WORK;
+};
+
+PomodoroTimer.prototype.runPollCallback = function() {
+    var currentStatus = {
+        totalSeconds: this.timer,
+        totalMinRestSeconds: this.minRestTimer,
+        canStartNextRound: this.canStartNextRound,
+        mode: this.getMode(),
+        modeString: this.modeString(),
+        status: this.getStatus(),
+        statusString: this.statusString(),
+        roundCount: this.roundCount
+    };
+
+    this.updateCallback(currentStatus);
+};
+
+PomodoroTimer.prototype.internalPause = function() {
+    this.status = PomodoroTimer.STATUS_PAUSED;
+    if (this.intervalId) {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    }
+};
+
+PomodoroTimer.prototype.internalReset = function() {
+    this.roundCount = 0;
+    this.canStartNextRound = false;
+    this.mode = null;
+    this.status = PomodoroTimer.STATUS_PAUSED;
+    this.setupNextRound();
+};
+
+
+// Public Methods
+
+PomodoroTimer.prototype.setTimerPatterns = function(timerPatterns) {
+    this.timerPatterns = timerPatterns;
+    this.reset();
+};
+
+PomodoroTimer.prototype.reset = function() {
+    this.internalPause();
+    this.internalReset();
+    this.runPollCallback();
+};
+
+PomodoroTimer.prototype.getMode = function() {
+    return this.mode;
+};
+
+PomodoroTimer.prototype.modeString = function() {
+    return PomodoroTimer.MODE_STRINGS[this.mode];
+};
+
+PomodoroTimer.prototype.getStatus = function() {
+    return this.status;
+};
+
+PomodoroTimer.prototype.statusString = function() {
+    return PomodoroTimer.STATUS_STRINGS[this.status];
+};
+
+PomodoroTimer.prototype.start = function() {
+    this.internalReset();
+    this.resume();
+};
+
+PomodoroTimer.prototype.pause = function() {
+    this.internalPause();
+    this.runPollCallback();
+};
+
+PomodoroTimer.prototype.resume = function() {
+    this.status = PomodoroTimer.STATUS_RUNNING;
+    this.runPollCallback();
+    if (!this.intervalId) {
+        var self = this;
+        this.intervalId = window.setInterval(function () {
+
+            if (--self.timer <= 0) {
+                if (self.mode === PomodoroTimer.MODE_WORK) {
+                    // Reset for restMaxTime
+                    self.mode = PomodoroTimer.MODE_REST;
+                    self.timer = self.restMaxTime;
+                }
+                else if (self.mode === PomodoroTimer.MODE_REST) {
+                    // We are done, start the next round!
+                    self.setupNextRound();
+                }
+            }
+
+            // If in rest mode, check for if we can start the next round once restMinTime was reached.
+            // Check if timer is less than the difference between restMaxTime and restMinTime
+            self.canStartNextRound = self.mode === PomodoroTimer.MODE_REST && self.timer <= (self.restMaxTime - self.restMinTime);
+
+            // Calculate min rest times
+            if (self.mode === PomodoroTimer.MODE_REST) {
+                if (self.timer > self.restMinTime) {
+                    self.minRestTimer = self.timer - self.restMinTime;
+                }
+                else {
+                    self.minRestTimer = 0;
+                }
+            }
+            else {
+                self.minRestTimer = null;
+            }
+
+            self.runPollCallback();
+        }, 1000);
+    }
+};
+
+PomodoroTimer.prototype.next = function() {
+    this.setupNextRound();
 }
